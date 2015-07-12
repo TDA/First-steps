abb24279b6e13a6f954c8807ef4dfad09ac6b980
Sai Pc <schand31@asu.edu>
2015-07-11 20:54:28 -0700

    Strings

---
+++ src/Strings.java
@@ -0,0 +1,16 @@
+import jdk.nashorn.internal.runtime.regexp.RegExp;
+
+/**
+ * Created by schandramouli on 7/11/15.
+ */
+public class Strings {
+    public static void main(String[] args){
+        StringBuilder b = new StringBuilder();
+        for(int i = 0 ; i < 10; i++)
+            b.append(i);
+
+        System.out.println(b.toString());
+        System.out.println(b.indexOf("7"));
+        System.out.println("sdf".matches("sdf"));
+    }
+}
