da55fe2d6cd9d5ef9ab79b33477239827d72d401
Sai Pc <schand31@asu.edu>
2015-09-06 23:19:51 -0700

    Added exception handling

--- src/BinarySearch.java
+++ src/BinarySearch.java
@@ -5,22 +5,30 @@ public class BinarySearch {
     public static int binarySearch(int[] array, int start, int end, int element) {
         // takes in an array with indices that are integers
         int mid = (start + end) / 2;
-
-        if(array[mid] == element) {
-            return mid;
-        } else if (element < array[mid]) {
-            return binarySearch(array, start, mid, element);
-        } else if (element > array[mid]) {
-            return binarySearch(array, mid + 1, end, element);
-        } else {
+        System.out.println("Searching from " + start + "to" + mid +"to"  + end);
+        try {
+            if (array[mid] == element) {
+                return mid;
+            } else if (element < array[mid]) {
+                return binarySearch(array, start, mid, element);
+            } else if (element > array[mid]) {
+                return binarySearch(array, mid + 1, end, element);
+            } else {
+                return -1;
+            }
+        } catch (ArrayIndexOutOfBoundsException e) {
             return -1;
         }
     }
 
     public static void main(String[] args) {
         int [] array = {1, 2, 3, 4, 5, 6};
-        int x = binarySearch(array, 0, array.length, 2);
-        System.out.println("Found at position " + x);
+        int x = binarySearch(array, 0, array.length, 11);
+        if (x == -1) {
+            System.out.println("Not found");
+        } else {
+            System.out.println("Found at position " + x);
+        }
     }
 
 }
