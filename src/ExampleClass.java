a85e8ebf00377f2e7d8c6e7c6e42690bc495bef3
Sai Pc <schand31@asu.edu>
2015-09-08 15:37:03 -0700

    Added some puzzles to exampleclass

--- src/ExampleClass.java
+++ src/ExampleClass.java
@@ -12,5 +12,15 @@ public class ExampleClass {
         int i = 2;
         String c[] = {"even", "odd"};
         System.out.println(c[i%2]);
+
+        int a = 5;
+        int b = 10;
+        System.out.println(a + " " + b);
+        a = a ^ b;
+        b = b ^ a;
+        a = a ^ b;
+        System.out.println(a + " " + b);
+        // I could have won, and I should have.
+        // But you did not, and that's all that matters.
     }
 }
