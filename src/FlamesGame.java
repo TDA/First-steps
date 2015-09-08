1f5a5c9d5c8e352bc769567049e0f4eb43e0fa2a
Sai Pc <schand31@asu.edu>
2015-09-07 23:30:55 -0700

    Flame game app added inputs

---
+++ src/FlamesGame.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+/**
+ * Created by schandramouli on 9/7/15.
+ */
+public class FlamesGame {
+    public static void main(String[] args) {
+        StringBuilder sb1 = new StringBuilder();
+        StringBuilder sb2 = new StringBuilder();
+        Scanner scanner = new Scanner(System.in);
+        sb1.append(scanner.nextLine());
+        sb2.append(scanner.nextLine());
+
+
+    }
+}
