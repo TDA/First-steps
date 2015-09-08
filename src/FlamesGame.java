dcfd029f8f1ba3b342c4691f391064dff30390e7
Sai Pc <schand31@asu.edu>
2015-09-08 00:33:37 -0700

    Completed initial version Added comments to change later

--- src/FlamesGame.java
+++ src/FlamesGame.java
@@ -9,16 +9,41 @@ public class FlamesGame {
         String s1 = "";
         String s2 = "";
         Scanner scanner = new Scanner(System.in);
-        s1 = scanner.nextLine();
-        s2 = scanner.nextLine();
+        s1 = scanner.nextLine().toLowerCase().replaceAll("\\s", "");
+        s2 = scanner.nextLine().toLowerCase().replaceAll("\\s", "");
+
+        // disregard duplicates --> One s removes all s and so on.
+        // need to change that to take into account dupes in the same name,
+        // as well as the second name :)
         HashMap<Character, Integer> hm = new HashMap<>();
         for (char c: s1.toCharArray()) {
-            if (hm.get(c) > 0) {
+            if (hm.get(c) != null) {
                 hm.put(c, hm.get(c) + 1);
             } else {
                 hm.put(c, 1);
             }
         }
-        System.out.println(hm);
+
+        for (char c: s2.toCharArray()) {
+            if (hm.get(c) != null && hm.get(c) > 0) {
+                hm.put(c, hm.get(c) - 1);
+            }
+        }
+        int matches = hm.size();
+        System.out.println(matches);
+        StringBuilder sb1 = new StringBuilder();
+        sb1.append("flames");
+        int i = 0;
+        while (sb1.length() > 1) {
+            i = i + matches - 1; // set the length of chars to skip
+            while (i >= sb1.length()) {
+                // reset i if its over the length
+                // helps to wrap around
+                i = i - sb1.length();
+            }
+            sb1.deleteCharAt(i);
+        }
+
+        System.out.println(sb1);
     }
 }
