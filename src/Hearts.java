c2fafbf9d1b6d5de5fa6166d4eeb76dcc8e6dba9
Sai Pc <schand31@asu.edu>
2015-09-09 12:17:47 -0700

    Added bottom triangle of heart, have to figure out the top semi circles, taking longer than i thought

--- src/Hearts.java
+++ src/Hearts.java
@@ -11,16 +11,39 @@ public class Hearts {
         System.out.println(s2);
     }
 
+    public static String centerLine(String s, int len) {
+        int spacing = (len - s.length()) / 2;
+        String new_s = "";
+        for (int i = 0; i < spacing; i++) {
+            new_s += " ";
+        }
+        new_s += s;
+        return new_s;
+    }
+
     public String generateheart() {
         String s = "";
-        int length = 23;
-        int height = 10;
-        for (int i = 0; i < height; i++){
+        int length = 21;
+        int height = 12;
+        for (int i = 0; i < (height / 2) - 2; i++){
             for (int j = 0; j < length; j++) {
                 s +=  "\u2665";
             }
             s +=  "\n";
         }
+
+        for (int i = 0; i < height / 2; i++){
+            int j = length - (i * 4);
+            String line = "";
+            for (;j > 0; j--) {
+                line = line + "\u2665";
+            }
+            // line and length anyone? :D
+            s += centerLine(line, length);
+            s +=  "\n";
+        }
+
+
         return s;
     }
 
@@ -28,8 +51,10 @@ public class Hearts {
         String s1 = "";
         String s2 = "";
         Scanner scanner = new Scanner(System.in);
-        s1 = scanner.nextLine();
-        s2 = scanner.nextLine();
+        //s1 = scanner.nextLine();
+        //s2 = scanner.nextLine();
+        s1 = args[0];
+        s2 = args[1];
 
         new Hearts().drawHeart(s1, s2);
 
