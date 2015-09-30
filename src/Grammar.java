600f9302b13af9a8ed447b4da087249f0049f303
Sai Pc <schand31@asu.edu>
2015-09-30 15:48:26 -0700

    Started coding grammars

---
+++ src/Grammar.java
@@ -0,0 +1,10 @@
+import jdk.nashorn.internal.ir.Symbol;
+
+/**
+ * Created by schandramouli on 9/30/15.
+ */
+public class Grammar {
+    Symbol[] Symbols;
+    Production[] productions;
+    Symbol start;
+}
