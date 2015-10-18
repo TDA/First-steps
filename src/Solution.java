0b3e959630f61336b05abbc4ba6eb8cf3b6794d0
Sai Pc <schand31@asu.edu>
2015-10-18 13:41:25 -0700

    Hackerrank tests

--- src/Solution.java
+++ src/Solution.java
@@ -5,5 +5,4 @@ public class Solution {
     public static void main(String[] args) {
 
     }
-
 }
--- src/Solution1.java
+++ src/Solution1.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 /**
  * Created by schandramouli on 8/26/15.
  */
 public class Solution1 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        String s1 = "";
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                s1 += " ";
+            }
+            for (int k = 0; k < i+1; k++) {
+                s1 += "#";
+            }
+            System.out.println(s1);
+            s1 = "";
+        }
+    }
 }
