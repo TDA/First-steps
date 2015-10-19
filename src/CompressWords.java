c9d5b4f4618e9de8cab43e86f90d7a46c049014d
Sai Pc <schand31@asu.edu>
2015-10-18 19:16:53 -0700

    Compress words with LinkedHashMap

--- src/CompressWords.java
+++ src/CompressWords.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Scanner;
 
 /**
@@ -5,8 +8,15 @@ import java.util.Scanner;
  */
 public class CompressWords {
     public static void main(String [] args) {
+
         Scanner scanner = new Scanner(System.in);
         String s = scanner.nextLine();
+        String a = compress(s);
+        System.out.println(a);
+        String b = compressWithHM(s);
+        System.out.println(b);
+    }
+    public static String compress(String s) {
         char currentLetter = s.charAt(0);
         int letterCount = 1;
         String finalString = "";
@@ -15,16 +25,37 @@ public class CompressWords {
             if(newLetter == currentLetter) {
                 letterCount++;
             } else {
-                finalString += currentLetter + "" + letterCount;
+                finalString += currentLetter + "";
+                if (letterCount > 1)
+                    finalString += letterCount;
                 letterCount = 1;
                 currentLetter = newLetter;
             }
         }
-        finalString += currentLetter + "" + letterCount;
-        if (! (finalString.length() < s.length())) {
-            System.out.println(s);
-        } else {
-            System.out.println(finalString);
+        finalString += currentLetter + "";
+        if (letterCount > 1)
+            finalString += letterCount;
+        return finalString;
+    }
+
+    public static String compressWithHM(String s) {
+        // Apparently LinkedHashMap maintains insertion order yay!
+        LinkedHashMap<Character, Integer> hm = new LinkedHashMap<>();
+        String finalString = "";
+        for (int i = 0; i < s.length(); i++) {
+            if(hm.containsKey(s.charAt(i))) {
+                hm.put(s.charAt(i), hm.get(s.charAt(i)) + 1);
+            } else {
+                hm.put(s.charAt(i), 1);
+            }
         }
+        System.out.println(hm);
+        for (Character character : hm.keySet()) {
+            finalString += character;
+            if(hm.get(character) > 1) {
+                finalString += hm.get(character);
+            }
+        }
+        return finalString;
     }
 }
