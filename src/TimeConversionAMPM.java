f0caaaa4fd672a30dac487e2cdb3d58984b73b61
Sai Pc <schand31@asu.edu>
2015-10-20 19:06:11 -0700

    Time conversion

--- src/TimeConversionAMPM.java
+++ src/TimeConversionAMPM.java
@@ -11,6 +11,29 @@ import java.util.regex.*;
  */
 public class TimeConversionAMPM {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String string = scanner.nextLine();
+        //System.out.println(string);
 
+        String pattern = "(\\d{2}):(\\d{2}):(\\d{2})(\\w{2})";
+
+        // Create a Pattern object
+        Pattern r = Pattern.compile(pattern);
+
+        // Now create matcher object.
+        Matcher m = r.matcher(string);
+
+        if (m.find( )) {
+            switch (m.group(4)) {
+                case "AM":
+                    System.out.println(m.group(1) + ":" + m.group(2) + ":" + m.group(3));
+                    break;
+                case "PM":
+                    System.out.println((Integer.parseInt(m.group(1)) + 12) + ":" + m.group(2) + ":" + m.group(3));
+                    break;
+            }
+        } else {
+            System.out.println("NO MATCH");
+        }
     }
 }
