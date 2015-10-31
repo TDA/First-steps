91b73eba7cfe8acd9955972f6a091d3aec87cda5
Sai Pc <schand31@asu.edu>
2015-10-31 14:51:14 -0700

    enhanced ans for godaddy interview

--- src/BalancedSum.java
+++ src/BalancedSum.java
@@ -5,14 +5,35 @@ public class BalancedSum {
     public static void main(String[] args) {
         int arr[] = {1, 2, 4, 3};
         System.out.println(CheckIfBalanced(arr));
+        System.out.println(EnchancedCheckIfBalanced(arr));
     }
     // this is what i wrote for the godaddy interview
+    public static int EnchancedCheckIfBalanced(int[] arr) {
+        int sumLeft = 0;
+        int sumRight = sum(arr);
+
+        for (int i = 0; i < arr.length; i++) {
+            sumRight -= arr[i];
+            if (sumLeft == sumRight) {
+                return 1;
+            }
+            sumLeft += arr[i];
+        }
+        return 0;
+    }
+
+    public static int sum (int[] arr) {
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+        }
+        return sum;
+    }
+
     public static int CheckIfBalanced(int[] arr) {
         for (int i = 0; i < arr.length; i++) {
             if (SumLeft(arr, i) == SumRight(arr, i)) {
                 return 1;
-            } else {
-                continue;
             }
         }
         return 0;
