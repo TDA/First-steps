5ee077d36bb24a4405bd3ce0fb9ee785e5f36c2a
Sai Pc <schand31@asu.edu>
2015-10-31 00:59:26 -0700

    getting somewhere with stack sorting

--- src/StackSorting.java
+++ src/StackSorting.java
@@ -6,13 +6,35 @@ import java.util.Stack;
 public class StackSorting {
     // Objective is to sort a stack (with biggest element on top)
     // Constrains: Cannot use more than 1 temp stack, no arrays or other DS
+    // temp variables allowed :O
     public static void main(String[] args) {
         Stack<Integer> intStack = new Stack<>();
+        Stack<Integer> tempStack = new Stack<>();
         // push them in reverse
-        for (int i = 10; i > 0; i--) {
+        /*for (int i = 10; i > 0; i--) {
             intStack.push(i);
-        }
+        }*/
+
         for (Integer i : intStack) {
+            System.out.println(i);
+        }
+        // lets try with one stack, using the temp stack when needed,
+        // bubble sort maybe?
+        while (! intStack.isEmpty()){
+            int temp = intStack.pop();
+            // check if lesser than next elt
+            if (intStack.peek() != null && intStack.peek() > temp) {
+                // we need to switch these
+                tempStack.push(temp);
+                tempStack.push(intStack.pop());
+            }
+            /*if (tempStack.isEmpty()) {
+                tempStack.push(intStack.pop());
+            } else {
+
+            }*/
+        }
+        for (Integer i : tempStack) {
             System.out.println(i);
         }
     }
