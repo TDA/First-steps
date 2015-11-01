fcd3f3edb25c475bc1e8bae6b7a45e185b290f21
Sai Pc <schand31@asu.edu>
2015-11-01 03:41:09 -0800

    fixed abs value and reading full string with newlines etc

--- src/FunnyStrings.java
+++ src/FunnyStrings.java
@@ -6,27 +6,32 @@ import java.util.Scanner;
 public class FunnyStrings {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
+        int n = Integer.parseInt(scanner.nextLine());
+        //System.out.println(n);
 
         for (int i = 0; i < n; i++) {
             String s = scanner.nextLine();
             // find the ascii difference of each set fo chars and check if they are equal,
             // if equal, go onto next letter seq, if not, break and return false
+            //System.out.println(s);
             int k = s.length();
+            System.out.println(k);
             // for 1 to n - 1, check this
             int j = 1;
             for (; j <= k - 1; j++) {
-                //System.out.println("first two " + s.charAt(j) + " " + s.charAt(j - 1));
-                //System.out.println("last two " + s.charAt(k - j) + s.charAt(k - j - 1));
-                if (s.charAt(j) - s.charAt(j - 1) == s.charAt(k - j) - s.charAt(k - j - 1)) {
+                System.out.println("first two " + s.charAt(j) + " " + s.charAt(j - 1));
+                System.out.println("first two " + (s.charAt(j) - s.charAt(j - 1)));
+                System.out.println("last two " + s.charAt(k - j) + s.charAt(k - j - 1));
+                System.out.println("last two " + (s.charAt(k - j) - s.charAt(k - j - 1)));
+                if (Math.abs(s.charAt(j) - s.charAt(j - 1)) == Math.abs(s.charAt(k - j) - s.charAt(k - j - 1))) {
                     continue;
                 } else {
-                    System.out.println("Not funny");
+                    System.out.println("Not Funny");
                     break;
                 }
             }
             // if it came here and j == k - 1, means all were equal
-            if (j == k - 1) {
+            if (j == k) {
                 System.out.println("Funny");
             }
 
