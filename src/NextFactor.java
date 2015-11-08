e0c5d2780942820c53293b11247e7181c0e94695
Sai Pc <schand31@asu.edu>
2015-11-08 02:40:50 -0700

    lolz

--- src/NextFactor.java
+++ src/NextFactor.java
@@ -13,23 +13,31 @@ public class NextFactor {
         System.out.println(Arrays.toString(primeFactorArray));
         // set an upper bound on the next number
         int upperBound = num * primeFactorArray[0];
+        // im sure this can be simplified as follows:
+        // find lcm of the factors, and add it to the number :\
+        // i think i over complicated this
         System.out.printf("Next number divisible by these factors should be between %s and %s\n", num, upperBound);
-        int computations = 0;
-        int i;
-        for (i = num + primeFactorArray[0]; i <= upperBound; i += primeFactorArray[0]) {
-            int count = 0;
-            for (int j = 0; j < primeFactorArray.length; j++) {
-                computations++;
-                if (i % primeFactorArray[j] == 0) {
-                    count++;
-                }
-            }
-            if (count == primeFactorArray.length) {
-                break;
-            }
+//        int computations = 0;
+//        int i;
+//        for (i = num + primeFactorArray[0]; i <= upperBound; i += primeFactorArray[0]) {
+//            int count = 0;
+//            for (int j = 0; j < primeFactorArray.length; j++) {
+//                computations++;
+//                if (i % primeFactorArray[j] == 0) {
+//                    count++;
+//                }
+//            }
+//            if (count == primeFactorArray.length) {
+//                break;
+//            }
+//        }
+        // was right, super simple
+        int lcm = 1;
+        for (Integer i : primeFactorArray) {
+            lcm *= i;
         }
-        System.out.printf("Next number is %s\n", i);
-        System.out.printf("Total computations is %s", computations);
+        System.out.printf("Next number is %s\n", num + lcm);
+//        System.out.printf("Total computations is %s", computations);
     }
     public static int[] factorize(int number) {
         ArrayList<Integer> primeFactorArrayList = new ArrayList<>();
