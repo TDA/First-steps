0e0979a9a5ed8da9073205d4e19fd196be3ae5b7
Sai Pc <schand31@asu.edu>
2015-11-12 22:43:49 -0700

    fixed the max sub set to find solution in O(n)

--- src/BalancedSum.java
+++ src/BalancedSum.java
@@ -7,7 +7,7 @@ public class BalancedSum {
         System.out.println(CheckIfBalanced(arr));
         System.out.println(EnchancedCheckIfBalanced(arr));
     }
-    // this is what i wrote for the godaddy interview
+
     public static int EnchancedCheckIfBalanced(int[] arr) {
         int sumLeft = 0;
         int sumRight = sum(arr);
@@ -22,7 +22,7 @@ public class BalancedSum {
         return 0;
     }
 
-    public static int sum (int[] arr) {
+    static int sum(int[] arr) {
         int sum = 0;
         for (int i = 0; i < arr.length; i++) {
             sum += arr[i];
@@ -30,6 +30,7 @@ public class BalancedSum {
         return sum;
     }
 
+    // this is what i wrote for the godaddy interview
     public static int CheckIfBalanced(int[] arr) {
         for (int i = 0; i < arr.length; i++) {
             if (SumLeft(arr, i) == SumRight(arr, i)) {
--- src/MaxSubSet.java
+++ src/MaxSubSet.java
@@ -3,20 +3,42 @@
  */
 public class MaxSubSet {
     // given an array, find the maximum continuous subset of it.
-    static int[] array = {-100, 10, -10, 5, -12, 13, -16, -3000};
+    static int[] array = {-100, 10, -9, 13, -12, 13, -16, -3000};
 
     public static void main(String[] args){
-        int current_total = 0;
-        int max = 0;
-
-        max = array[0];
+        int current_total = BalancedSum.sum(array);
         // i dont believe this would work tho, or maybe the program has different constraints?
-        for(int i = 0; i<array.length ; ++i){
-            current_total+=array[i];
-            if(current_total > max) max = current_total;
-            // no need to check for equality, if its zero, its zero
-            if(current_total < 0) current_total = 0;
+        // we need to be able to find a sum, and then check if its the most
+        // lets sum the array, then keep removing -negatives from each end till we cant have anymore.
+        // is bound to give a reasonably good result, except when the -negative in middle is HUGE
+        int i = 0;
+        while (array[i] < 0) {
+            // addition by subtraction
+            current_total = current_total - array[i];
+            i++;
         }
-        System.out.println(max);
+        int j = array.length - 1;
+        while (array[j] < 0) {
+            // addition by subtraction
+            current_total = current_total - array[j];
+            j--;
+        }
+
+        // now we have potentially a big nice subset, now again we need to iterate
+        // and remove positive+negative combinations that result in negative values
+        // this will give us the answer, and its still O(n)
+        while (i < j && (array[i] + array[i + 1]) < 0) {
+            // addition by subtraction
+            current_total = current_total - array[i] - array[i + 1];
+            i = i + 2;
+        }
+
+        while (j > i && (array[j] + array[j - 1]) < 0) {
+            // addition by subtraction
+            current_total = current_total - array[j] - array[j - 1];
+            j = j - 2;
+        }
+
+        System.out.println(current_total);
     }
 }
