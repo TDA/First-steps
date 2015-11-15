c152dae9d97a6a899ad35aac1d5321d0fcd3fec8
Sai Pc <schand31@asu.edu>
2015-11-14 23:46:46 -0700

    find largest fibonacci within the given limit

---
+++ src/FindFibonacciNumber.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+/**
+ * Created by schandramouli on 11/14/15.
+ */
+public class FindFibonacciNumber {
+    public static void main(String[] args) {
+        int first = 0;
+        int second = 1;
+        Scanner scanner = new Scanner(System.in);
+        int fibonacciLimit = scanner.nextInt();
+        while (first + second < fibonacciLimit) {
+            int temp = first + second;
+            first = second;
+            second = temp;
+        }
+        System.out.printf("The largest fibonacci number below %d is %d", fibonacciLimit, second);
+    }
+}
