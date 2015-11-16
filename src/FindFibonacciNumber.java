d2f77782bb206b59d69192e3460aa18568c6f806
Sai Pc <schand31@asu.edu>
2015-11-16 11:45:30 -0700

    fibonacci on steroids

--- src/FindFibonacciNumber.java
+++ src/FindFibonacciNumber.java
@@ -3,19 +3,36 @@ import java.util.Scanner;
 /**
  * Created by schandramouli on 11/14/15.
  */
+enum Type {
+    Iterative,
+    Recursive,
+    DP
+}
 public class FindFibonacciNumber {
     public static void main(String[] args) {
-        int first = 0;
-        int second = 1;
+        // need to expand this
         Scanner scanner = new Scanner(System.in);
         int fibonacciLimit = scanner.nextInt();
-        while (first + second < fibonacciLimit) {
-            int temp = first + second;
-            first = second;
-            second = temp;
-        }
+        int second = fibonacci(fibonacciLimit, Type.Iterative);
         System.out.printf("The largest fibonacci number below %d is %d", fibonacciLimit, second);
     }
-}
 
-asdvasdv
+    static int fibonacci(int n, Type t) {
+        // find the nth fibonacci by iteration
+        if (t == Type.Iterative) {
+            int first = 0;
+            int second = 1;
+            while (first + second < n) {
+                int temp = first + second;
+                first = second;
+                second = temp;
+            }
+            return second;
+        } else {
+            return 0;
+        }
+
+    }
+
+
+}
