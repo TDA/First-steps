b8c5824a2ec06d56af207f94112824361c74d29d
Sai Pc <schand31@asu.edu>
2015-11-16 21:20:04 -0700

    kid jump with dp

--- src/KidJumpingSteps.java
+++ src/KidJumpingSteps.java
@@ -1,17 +1,25 @@
+import java.util.HashMap;
 import java.util.Scanner;
 
 /**
  * Created by schandramouli on 11/16/15.
  */
 public class KidJumpingSteps {
+    static HashMap<Integer, Integer> hm = new HashMap<>();
+    static int recCount;
+    static int dpCount;
     // kid can jump 1 2 or 3 steps at a time, count total ways of jumping n steps
     public static void main(String[] args) {
+        // hm cache
+        hm.put(0, 1);
         Scanner scanner = new Scanner(System.in);
         int steps = scanner.nextInt();
-        System.out.println(calculateJumps(steps));
+        System.out.println(calculateJumps(steps) + " in " + recCount);
+        System.out.println(calculateJumpsDP(steps) + " in " + dpCount);
     }
 
     static int calculateJumps(int n) {
+        recCount++;
         // base cases, 1, 2, 3
         if (n < 0) {
             return 0;
@@ -21,5 +29,22 @@ public class KidJumpingSteps {
             return 1;
         }
         return calculateJumps(n - 1) + calculateJumps(n - 2) + calculateJumps(n - 3);
+    }
+
+    static int calculateJumpsDP(int n) {
+        // we cache stuff
+        dpCount++;
+        // if already cached, return that
+        if (hm.containsKey(n)) {
+            return hm.get(n);
+        } else {
+            // not cached, need to calculate
+            if (n < 0) {
+                return 0;
+            }
+            int jumps = calculateJumpsDP(n - 1) + calculateJumpsDP(n - 2) + calculateJumpsDP(n - 3);
+            hm.put(n, jumps);
+            return jumps;
+        }
     }
 }
