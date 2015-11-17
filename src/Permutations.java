c6d0486c43a8d8c35455ba99927c26c549a5f8e2
Sai Pc <schand31@asu.edu>
2015-11-17 00:45:33 -0700

    permutations

---
+++ src/Permutations.java
@@ -0,0 +1,45 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by schandramouli on 11/17/15.
+ */
+public class Permutations<T> {
+    static Set<String> stringSet = new HashSet<>();
+    static int recCount;
+    public static void main(String[] args) {
+        String s = "abcd";
+        Permutations<String> permutations = new Permutations<>();
+        System.out.println(permutations.findPermutations(s));
+        System.out.println(recCount);
+    }
+
+    ArrayList findPermutations(String string) {
+        recCount++;
+        ArrayList<String> stringArrayList = new ArrayList<>();
+        // just a letter, so return it
+        if (string.length() == 1) {
+            stringArrayList.add(string);
+            return stringArrayList;
+        }
+        // else get the first character, and
+        // find the permutations of remaining characters,
+        // and insert the first character into each of these permutations
+        char first = string.charAt(0);
+        String remainderString = string.substring(1);
+        // find perms of remaining ones, and insert first char into each of those
+        ArrayList<String> tempStringArrayList = this.findPermutations(remainderString);
+        for (String s : tempStringArrayList) {
+            for (int i = 0; i <= s.length(); i++) {
+                String newString = insertCharAt(s, first, i);
+                stringArrayList.add(newString);
+            }
+        }
+        return stringArrayList;
+    }
+
+    static String insertCharAt(String s, char letter, int position) {
+        return s.substring(0, position) + letter + s.substring(position, s.length());
+    }
+}
