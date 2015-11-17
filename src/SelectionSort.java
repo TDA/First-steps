96d2ec4685bdfe98d5d79f2427e02141a826ec5d
Sai Pc <schand31@asu.edu>
2015-11-17 02:23:45 -0700

    played around with some sorts

--- src/SelectionSort.java
+++ src/SelectionSort.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class SelectionSort {
  
     public static int[] doSelectionSort(int[] arr){
@@ -5,24 +7,31 @@ public class SelectionSort {
         for (int i = 0; i < arr.length - 1; i++)
         {
             int index = i;
-            for (int j = i + 1; j < arr.length; j++)
+            for (int j = i + 1; j < arr.length; j++) {
+                count++;
+                System.out.println(Arrays.toString(arr));
                 if (arr[j] < arr[index])
                     index = j;
-      
+            }
             int smallerNumber = arr[index]; 
             arr[index] = arr[i];
             arr[i] = smallerNumber;
+
         }
         return arr;
     }
-     
+
+    static int count;
     public static void main(String a[]){
          
-        int[] arr1 = {10,34,2,56,7,67,88,42};
+        int[] arr1 = {55, 10, 34, 26, 56, 7, 67, 88, 42};
+        //int[] arr1 = {3, 4, 5, 6, 7, 8, 9, 2};
         int[] arr2 = doSelectionSort(arr1);
         for(int i:arr2){
             System.out.print(i);
             System.out.print(", ");
         }
+        System.out.println();
+        System.out.println("total iterations" + count);
     }
 }
