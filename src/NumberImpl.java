1820e235bc909528cf55ded7cbaf579c44c435e7
Sai Pc <schand31@asu.edu>
2015-11-17 23:02:20 -0700

    finished number impl

---
+++ src/NumberImpl.java
@@ -0,0 +1,37 @@
+/**
+ * Created by schandramouli on 11/17/15.
+ */
+public abstract class NumberImpl extends Number{
+//    double value;
+//
+//    public NumberImpl() {
+//        this.value = 0;
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return this.value + "";
+//    }
+//
+//    @Override
+//    public double doubleValue() {
+//        return this.value;
+//    }
+//
+//    @Override
+//    public float floatValue() {
+//        return (float) (this.value);
+//    }
+//
+//    @Override
+//    public long longValue() {
+//        return (long) (this.value);
+//    }
+//
+//    @Override
+//    public int intValue() {
+//        return (int) (this.value);
+//    }
+
+    public abstract int compareTo(NumberImpl o);
+}
