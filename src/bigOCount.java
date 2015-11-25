5991fa37bf2d56b0e57ab148bff50b86d8212dc2
Sai Pc <schand31@asu.edu>
2015-11-25 00:17:14 -0700

    big o analysis

---
+++ src/bigOCount.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+/**
+ * Created by schandramouli on 11/26/15.
+ */
+public class bigOCount {
+    // for each program fed in as input, calculate the big o running complexity
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int count = 0;
+        while (scanner.hasNext()) {
+            // read in line by line
+            String line = scanner.nextLine();
+            if (isStatement(line) ||)
+        }
+    }
+}
