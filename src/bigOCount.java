07e8f841882f361f729a553721dfb67acd90117b
Sai Pc <schand31@asu.edu>
2015-11-26 00:48:26 -0700

    good progess on big o

--- src/bigOCount.java
+++ src/bigOCount.java
@@ -1,3 +1,4 @@
+import java.io.*;
 import java.util.Scanner;
 
 /**
@@ -5,31 +6,47 @@ import java.util.Scanner;
  */
 enum TYPES {
     STATEMENT,
-    LOOP
+    LOOP,
+    COMMENT,
+    EMPTY
 }
 public class bigOCount {
     // for each program fed in as input, calculate the big o running complexity
     // this is a simple one that doesnt think much about loops
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException, IOException{
         Scanner scanner = new Scanner(System.in);
+        File f1 = new File("src/bignumbers.java");
+        BufferedReader buff = new BufferedReader(new FileReader(f1));
         int count = 0;
-        while (scanner.hasNext()) {
+        String line;
+        while ((line = buff.readLine()) != null) {
             // read in line by line
-            String line = scanner.nextLine();
             switch (findType(line)) {
                 case STATEMENT: count++;
                     break;
                 case LOOP:
                     // add logic here to find the amount of looping required, nested loops etc
                     break;
+                case COMMENT:
+                    // do nothing
+                    break;
             }
-
-
-
         }
+        System.out.println("Big o was " + count);
     }
 
     public static TYPES findType(String line) {
+        // java and its double slashes :|
+        System.out.println(line);
+        // skip empty lines
+        if (line.matches("^\\s*$")) {
+            return TYPES.EMPTY;
+        }
+
+        // ignore comments
+        if (line.matches("^(//|/\\*|\\s*\\*|(import)).*")) {
+            return TYPES.COMMENT;
+        }
         if (line.matches("for|while|do")) {
             return TYPES.LOOP;
         } else {
