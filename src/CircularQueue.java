96c8bad118a0e118aa4b32bd6f7fe4ddbed10283
Sai Pc <schand31@asu.edu>
2015-11-28 23:17:10 -0700

    circular queue with generics

---
+++ src/CircularQueue.java
@@ -0,0 +1,17 @@
+import java.util.Comparator;
+
+/**
+ * Created by schandramouli on 11/28/15.
+ */
+public class CircularQueue<T> implements Comparable {
+
+
+    @Override
+    public int compareTo(Object o) {
+        return 0;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
