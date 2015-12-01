03a07efa8053824945c8ce29319cc13abd114139
Sai Pc <schand31@asu.edu>
2015-12-01 01:57:16 -0700

    quicksort fixed

--- src/QuickSort.java
+++ src/QuickSort.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
  * Created by schandramouli on 11/17/15.
  */
 public class QuickSort {
-    static int[] array = {13, 12, 11, 10, 9, 8, 2, 15, 11, 9};
+    static int[] array = {13, 12, 11, 10, 9, 8, 2, 15, 11, 9, 8};
     static int count;
     // find a pivot, and shift elements lesser than it to left, and greater than it to right
     public static void main(String[] args) {
@@ -14,21 +14,25 @@ public class QuickSort {
     }
 
     static int partition (int arr[], int left, int right) {
-        int pivot = (left + right) / 2;
+        int pivot = arr[(left + right) / 2];
         int i = left;
         int j = right;
-        while (arr[i] < arr[pivot]) {
-            i++;
-        }
-        while (arr[j] > arr[pivot]) {
-            j--;
-        }
+        while (i <= j) {
+            while (arr[i] < pivot) {
+                i++;
+            }
+            while (arr[j] > pivot) {
+                j--;
+            }
 
-        if (i <= j) {
-            // switch the elements
-            int temp = arr[j];
-            arr[j] = arr[i];
-            arr[i] = temp;
+            if (i <= j) {
+                // switch the elements
+                int temp = arr[j];
+                arr[j] = arr[i];
+                arr[i] = temp;
+                i++;
+                j--;
+            }
         }
         return i;
     }
@@ -37,12 +41,12 @@ public class QuickSort {
         System.out.println(++count);
         int pivot = partition(arr, left, right);
 
-        if (left < pivot) {
+        if (left < pivot - 1) {
             // recursively break down the array
-            quickSort(arr, left, pivot);
+            quickSort(arr, left, pivot - 1);
         }
-        if ((pivot + 1) < right) {
-            quickSort(arr, pivot + 1, right);
+        if (pivot < right) {
+            quickSort(arr, pivot, right);
         }
     }
 }
