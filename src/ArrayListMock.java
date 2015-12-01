8d63d8566a92d70a69a5117f2b2bb2275ae7b6ed
Sai Pc <schand31@asu.edu>
2015-11-30 22:18:34 -0700

    arraylist done to an extent

--- src/ArrayListMock.java
+++ src/ArrayListMock.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /**
  * Created by schandramouli on 11/30/15.
  */
@@ -25,8 +27,40 @@ public class ArrayListMock<T> {
         }
     }
 
+    public T get(int index) {
+        if (index < size) {
+            return array[index];
+        } else {
+            System.out.println("Array index out of bounds");
+            return (T) null;
+        }
+    }
+
+    public int length() {
+        return currentPointer;
+    }
+
+    @Override
+    public String toString() {
+        return "ArrayListMock{" +
+                "array=" + Arrays.toString(array) +
+                ", currentPointer=" + currentPointer +
+                ", size=" + size +
+                ", length=" + length() +
+                '}';
+    }
+
     public static void main(String[] args) {
         // driver pgm
+        ArrayListMock<Integer> arrayListMock = new ArrayListMock<>(3);
+        arrayListMock.add(1);
+        arrayListMock.add(2);
+        arrayListMock.add(3);
+        arrayListMock.add(4);
+        arrayListMock.add(5);
+        arrayListMock.add(5);
+        System.out.println(arrayListMock);
+        System.out.println(arrayListMock.get(2));
 
     }
 }
