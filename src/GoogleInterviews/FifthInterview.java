c352bb9571dfed93ee19fcd20127e06ee1c31cb2
Sai Pc <schand31@asu.edu>
2015-12-03 10:52:16 -0700

    all interviews

---
+++ src/GoogleInterviews/FifthInterview.java
@@ -0,0 +1,7 @@
+package GoogleInterviews;
+
+/**
+ * Created by schandramouli on 12/3/15.
+ */
+public class FifthInterview {
+}
---
+++ src/GoogleInterviews/SecondInterview.java
@@ -0,0 +1,19 @@
+package GoogleInterviews;
+
+/**
+ * Created by schandramouli on 12/3/15.
+ */
+public class SecondInterview {
+    // The game :O :|
+//    1. Given a string of dashes and pluses, generate all possible
+//       combinations of legal moves
+//       a. legal move is when two dashes are adjacent to each other, they can be flipped to pluses
+//    2. Given a string of dashes and pluses, write a "isWin" method, that will return true if
+//       player one can win, else returns false.
+//       a. If a player has no legal moves at the start of his turn => he wins.
+
+    public static void main(String[] args) {
+
+    }
+
+}
