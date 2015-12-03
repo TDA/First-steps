36d8830998fb6bdd120ada67cd8b618641409957
Sai Pc <schand31@asu.edu>
2015-12-02 23:55:35 -0700

    first commit to GI

--- src/GoogleInterviews/FirstInterview.java
+++ src/GoogleInterviews/FirstInterview.java
@@ -4,5 +4,10 @@ package GoogleInterviews;
  * Created by schandramouli on 12/2/15.
  */
 public class FirstInterview {
-
+    // given an iterator of iterators, implement next() and hasNext()
+    // to get the iterators in alternating fashion
+//    [0] -> a1, a2, a3
+//    [1] -> b1, b2
+//    [2] -> c1, c3
+//    => a1, b1, c1, a2, b2, a3, c3 should be the output :)
 }
