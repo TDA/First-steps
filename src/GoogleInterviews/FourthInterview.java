d417894072ec871283c76f721962ce63333867f8
Sai Pc <schand31@asu.edu>
2015-12-05 22:52:07 -0700

    3rd and 4th

--- src/GoogleInterviews/FourthInterview.java
+++ src/GoogleInterviews/FourthInterview.java
@@ -4,4 +4,7 @@ package GoogleInterviews;
  * Created by schandramouli on 12/3/15.
  */
 public class FourthInterview {
+    // Convert a binary tree into a circular doubly linked list
+    // no restriction on the ordering of elements
+    // "Attention to detail" question :D
 }
--- src/GoogleInterviews/ThirdInterview.java
+++ src/GoogleInterviews/ThirdInterview.java
@@ -4,4 +4,9 @@ package GoogleInterviews;
  * Created by schandramouli on 12/3/15.
  */
 public class ThirdInterview {
+    // Given a list/set of input ranges, output only the ranges which do
+    // not overlap with any other range. (i.e)
+    // 0-10, 8-15, 17-20, 22-30, 28-40
+    // => Ouput is 17-20 since it doesnt overlap with anything else
+    // Everything else overlaps with something else, so can be discounted :)
 }
