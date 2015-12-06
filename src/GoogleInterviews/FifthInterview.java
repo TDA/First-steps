80ce6cc61edfff71482fe7a8408110619e7c4d69
Sai Pc <schand31@asu.edu>
2015-12-05 23:00:30 -0700

    fifth

--- src/GoogleInterviews/FifthInterview.java
+++ src/GoogleInterviews/FifthInterview.java
@@ -4,4 +4,9 @@ package GoogleInterviews;
  * Created by schandramouli on 12/3/15.
  */
 public class FifthInterview {
+    // Given: a Server that can handle requests from external sources
+    // Server can parse XML => internal tree representation
+    // Server is receiving a lot of XML docs that could potentially
+    // be duplicated and need to be optimized in terms of storage.
+    // Basically cache the duplicated nodes, and re-build them as needed.
 }
