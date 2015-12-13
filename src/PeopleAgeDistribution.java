2c24ab35a50b0b1892635ad2a5dd701b47f90d5e
Sai Pc <schand31@asu.edu>
2015-12-13 22:45:12 +0530

    People ages

--- src/PeopleAgeDistribution.java
+++ src/PeopleAgeDistribution.java
@@ -6,6 +6,20 @@ public class PeopleAgeDistribution {
         // given a list of start and end (birth and death dates) of people
         // find which year has the most number of people born in it :D
         // hashmap i think?
-
+        HashMap<Integer, Integer> peopleBirths = new HashMap<>();
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter the number of people");
+        int n = Integer.parseInt(s.nextLine());
+        for (int i = 0; i < n; i++) {
+        	int birthYear = s.nextInt();
+        	int deathYear = s.nextInt();
+            if (map.containsKey(birthYear)) {
+                int x = map.get(birthYear);
+                map.put(birthYear, ++x);
+            } else {
+                map.put(birthYear, 1);
+            }
+        }
+        System.out.println(peopleBirths);
     }
 }
