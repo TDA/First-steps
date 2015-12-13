719f88a7aee24e7fb3f3c13e7bf40a7e1e4dc8ed
Sai Pc <schand31@asu.edu>
2015-12-12 22:20:41 -0700

    people distro

--- src/PeopleAgeDistribution.java
+++ src/PeopleAgeDistribution.java
@@ -5,6 +5,7 @@ public class PeopleAgeDistribution {
     public static void main(String[] args) {
         // given a list of start and end (birth and death dates) of people
         // find which year has the most number of people born in it :D
+        // hashmap i think?
 
     }
 }
