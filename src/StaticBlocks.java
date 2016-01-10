71adb1037df0795820d2832f3c92957d7ffe25f6
Sai Pc <schand31@asu.edu>
2016-01-10 23:16:45 +0530

    Static blocks

---
+++ src/StaticBlocks.java
@@ -0,0 +1,13 @@
+/**
+ * Created by schandramouli on 1/10/16.
+ */
+public class StaticBlocks {
+    static {
+        System.out.println("This is a static block");
+        System.out.println("That executes without requiring gany 'main' program");
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
