4dd97d6c957092c5d1dfe4f57ae333aab5235753
Sai Pc <schand31@asu.edu>
2016-01-10 23:25:08 +0530

    Working impl of def-hashmaps

--- src/DefaultHashMap.java
+++ src/DefaultHashMap.java
@@ -3,6 +3,8 @@ import java.util.HashMap;
 /**
  * Created by schandramouli on 1/10/16.
  */
+//"Implementation of default hashmaps like from ruby"
+
 public class DefaultHashMap<K, V> extends HashMap {
     // this is to simulate the default hashmaps of ruby/python
     protected V defaultValue;
@@ -14,6 +16,21 @@ public class DefaultHashMap<K, V> extends HashMap {
     @Override
     public Object get(Object key) {
         // return the value if it exists, else, return default value
-        return containsKey(key)? super.get(key) : defaultValue;
+        return  containsKey(key)? super.get(key) : defaultValue;
+    }
+
+    public static void main(String[] args) {
+        // test driver for defaulthashmaps
+        DefaultHashMap<Character, Integer> defaultHashMap = new DefaultHashMap<>(0);
+
+        String s = "abcdefghijklmnopqrstuvabcdef";
+
+        //seems to work
+        for (Character c: s.toCharArray()) {
+            defaultHashMap.put(c, (int) defaultHashMap.get(c) + 1);
+        }
+
+        System.out.println(defaultHashMap);
+
     }
 }
