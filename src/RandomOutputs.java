43983a02f694c7d400f468f8db988099f4d00487
Sai Pc <schand31@asu.edu>
2016-01-14 23:59:57 +0530

    random outputs program with math random

--- src/RandomOutputs.java
+++ src/RandomOutputs.java
@@ -4,5 +4,11 @@
 public class RandomOutputs {
     public static void main(String[] args) {
         // this will return random letters using a hashmap
+        DefaultHashMap<Integer, Integer> defaultHashMap = new DefaultHashMap<>(0);
+        for (int i = 0; i < 10; i++) {
+            defaultHashMap.put(i, Math.random() * 10);
+        }
+
+        System.out.println(defaultHashMap);
     }
 }
