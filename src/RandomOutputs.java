e4ef354f27e8dd25157a1f8ee23f068dbee99f6f
Sai Pc <schand31@asu.edu>
2016-01-14 23:57:13 +0530

    random outputs program

---
+++ src/RandomOutputs.java
@@ -0,0 +1,8 @@
+/**
+ * Created by schandramouli on 1/14/16.
+ */
+public class RandomOutputs {
+    public static void main(String[] args) {
+        // this will return random letters using a hashmap
+    }
+}
