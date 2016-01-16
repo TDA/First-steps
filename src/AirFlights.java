9fd5941e0867a29e5eef7cbe8375a61ef17af452
Sai Pc <schand31@asu.edu>
2016-01-16 23:57:28 +0530

    private variables for flighes

--- src/AirFlights.java
+++ src/AirFlights.java
@@ -3,6 +3,15 @@
  */
 public class AirFlights {
     // lets define a flight class
+    Engine engine;
+    int seatCapacity;
+    String type;
+    String flightName;
+    int width;
+    int height;
+    float maxSpeed;
+    String to;
+    String from;
     public static void main(String[] args) {
 
     }
