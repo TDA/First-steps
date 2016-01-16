d103061d437867269f47bb0d77acbcb03274fb4b
Sai Pc <schand31@asu.edu>
2016-01-17 00:12:07 +0530

    more mods to priv vars

--- src/Vehicles/AirFlights.java
+++ src/Vehicles/AirFlights.java
@@ -1,13 +1,17 @@
 package Vehicles;
+
+import javafx.geometry.Dimension2D;
+
 /**
  * Created by schandramouli on 1/16/16.
  */
 public class AirFlights {
     // lets define a flight class
     Engine engine;
+    // my argument is that max speed is determined by
+    // more than just the engine, so it needs to be
+    // declared here instead of in engine :)
     float maxSpeed;
-    int width;
-    int height;
     String flightName;
     String to;
     String from;
@@ -18,14 +22,20 @@ public class AirFlights {
     final int seatCapacity;
     // same for type of the flight
     final String type;
+    final int width;
+    final int height;
 
-    public AirFlights(Engine engine, int seatCapacity, String flightName, String type) {
+    public AirFlights(Engine engine, int seatCapacity, String flightName, String type, Dimension2D d) {
         // these fours things are not dependent on anything
         // other than creation
         this.engine = engine;
         this.seatCapacity = seatCapacity;
         this.flightName = flightName;
         this.type = type;
+        // width and height never change, and are prolly
+        // never used as well :D
+        this.width = ((int) d.getWidth());
+        this.height = ((int) d.getHeight());
     }
 
     public Engine getEngine() {
