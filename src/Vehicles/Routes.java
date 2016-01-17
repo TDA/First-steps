5e93c83490b301afb204d7d1815cbe06b85eb16d
Sai Pc <schand31@asu.edu>
2016-01-18 00:20:41 +0530

    Ok, time to refactor

--- src/Vehicles/Routes.java
+++ src/Vehicles/Routes.java
@@ -1,8 +1,12 @@
 package Vehicles;
 
+import java.util.ArrayList;
+
 /**
  * Created by schandramouli on 1/18/16.
  */
 public class Routes {
-
+    // this needs a collection of Cities in it, right?
+    // or more clearly airports
+    ArrayList<Airport> airports = new ArrayList<>();
 }
