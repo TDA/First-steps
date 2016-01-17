b72239b35590d28699c77992160020f765973ef0
Sai Pc <schand31@asu.edu>
2016-01-18 00:27:57 +0530

    consternation

--- src/AirflightSystem/Airport.java
+++ src/AirflightSystem/Airport.java
@@ -5,4 +5,8 @@ package AirflightSystem;
  */
 public class Airport {
     // this is a HUGE class i am telling you right now.
+    // let this be a stub for now, it needs to contain Terminals,
+    // Gates, Security checkpoints, Immigration stuff and so on
+    // Also, who governs the employees? is Airport a monolithic
+    // class that has everything or should we break it up?
 }
