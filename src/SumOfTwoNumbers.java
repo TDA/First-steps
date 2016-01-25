d39028a46b1a1637728fbad1e8d11bfa502d9602
Sai Pc <schand31@asu.edu>
2016-01-25 15:38:35 -0700

    hashmap added to twonumbers problem

--- src/SumOfTwoNumbers.java
+++ src/SumOfTwoNumbers.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.HashMap;
 
 /**
  * Created by schandramouli on 1/25/16.
@@ -6,9 +7,18 @@ import java.util.Arrays;
 public class SumOfTwoNumbers {
     public static void main(String[] args) {
         int[] array = {1, 5, 8, 3, 10, 12, 1, 6};
-        int[] copyArray = Arrays.copyOf(array, array.length);
-        int sum = 8;
+        int[] copy = Arrays.copyOf(array, array.length);
         Arrays.sort(array);
+
+        HashMap<Integer, Integer> hashMap = new HashMap<>();
+        for (int i = 0; i < array.length; i++) {
+            hashMap.put(i, Arrays.binarySearch(array, copy[i]));
+        }
+        System.out.println(Arrays.toString(array));
+        System.out.println(Arrays.toString(copy));
+        System.out.println(hashMap);
+        int sum = 8;
+
         int l = 0;
         int r = array.length - 1;
         while (l < r && (array[l] + array[r] != sum)) {
