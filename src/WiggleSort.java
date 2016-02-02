1e7a37ef73f5a233812c6d1215a0f515e1557ad8
Sai Pc <schand31@asu.edu>
2016-02-02 00:03:24 -0700

    wiggle

---
+++ src/WiggleSort.java
@@ -0,0 +1,9 @@
+/**
+ * Created by schandramouli on 2/3/16.
+ */
+public class WiggleSort {
+    public static void main(String[] args) {
+        // given an array, sort the array, with
+        // big small big small elements alternatively
+    }
+}
