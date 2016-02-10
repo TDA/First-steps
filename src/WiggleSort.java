2e44c6ec97e7448973ee38ebbb2933a811a8e1d7
Sai Pc <schand31@asu.edu>
2016-02-09 23:43:59 -0700

    started wiggle sort

--- src/WiggleSort.java
+++ src/WiggleSort.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /**
  * Created by schandramouli on 2/3/16.
  */
@@ -5,5 +7,11 @@ public class WiggleSort {
     public static void main(String[] args) {
         // given an array, sort the array, with
         // big small big small elements alternatively
+        int[] array = {1, 7, 8, 4, 3, 9};
+        int[] finalArray = new int[array.length];
+        // n log n solution is to sort and
+        // then use two pointers
+        Arrays.sort(array);
+        for ()
     }
 }
