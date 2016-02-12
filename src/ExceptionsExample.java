3bd067714ce9eafb93d241d0e1f533387df20ab6
Sai Pc <schand31@asu.edu>
2016-02-12 12:32:02 -0700

    added an exceptions example

---
+++ src/ExceptionsExample.java
@@ -0,0 +1,14 @@
+/**
+ * Created by schandramouli on 2/12/16.
+ */
+public class ExceptionsExample {
+    public static void main(String[] args) {
+        try {
+
+        } catch (Exception e) {
+
+        } finally {
+            //cleanup code
+        }
+    }
+}
