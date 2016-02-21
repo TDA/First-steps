88dfa96204ab93aa231af14b2c3681b4711bb218
Sai Pc <schand31@asu.edu>
2016-02-21 03:14:22 -0700

    Solved google 3rd round, need to optimize

---
+++ src/GoogleInterviews/Range.java
@@ -0,0 +1,80 @@
+package GoogleInterviews;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+/**
+ * Created by schandramouli on 2/13/16.
+ */
+public class Range implements Comparable {
+    long start;
+    long end;
+
+    public Range() {
+        this.start = 0;
+        this.end = 0;
+    }
+
+    public Range(long start, long end) {
+        this.start = start;
+        this.end = end;
+    }
+
+    public Range(String s) {
+        String[] times = s.split("-");
+        this.start = Long.parseLong(times[0]);
+        this.end = Long.parseLong(times[1]);
+    }
+
+    public boolean isOverlapping(Range s) {
+        // if either a ends before b or b ends
+        // before a, then they are NOT overlapping,
+        // else they are :O
+        return !(this.end < s.start || this.start > s.end);
+    }
+
+    public long getStart() {
+        return start;
+    }
+
+    public void setStart(long start) {
+        this.start = start;
+    }
+
+    public long getEnd() {
+        return end;
+    }
+
+    public void setEnd(long end) {
+        this.end = end;
+    }
+
+    // sorts based on start times
+    public static ArrayList<Range> sortRanges(ArrayList<Range> ranges) {
+        ArrayList<Range> ranges1 = ((ArrayList<Range>) ranges.clone());
+        Collections.sort(ranges1);
+        return ranges1;
+    }
+
+    @Override
+    public int compareTo(Object range) {
+        if (this.start == ((Range) range).start) {
+            if (this.end == ((Range) range).end) {
+                return 0;
+            } else if (this.end > ((Range) range).end) {
+                return 1;
+            } else {
+                return -1;
+            }
+        } else if (this.start > ((Range) range).start) {
+            return 1;
+        } else {
+            return -1;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "{ " + this.start + " - " + this.end + " }";
+    }
+}
--- src/GoogleInterviews/ThirdInterview.java
+++ src/GoogleInterviews/ThirdInterview.java
@@ -1,5 +1,12 @@
 package GoogleInterviews;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.LinkedHashMap;
+import java.util.Set;
+
+import GoogleInterviews.Range;
+
 /**
  * Created by schandramouli on 12/3/15.
  */
@@ -7,6 +14,77 @@ public class ThirdInterview {
     // Given a list/set of input ranges, output only the ranges which do
     // not overlap with any other range. (i.e)
     // 0-10, 8-15, 17-20, 22-30, 28-40
-    // => Ouput is 17-20 since it doesnt overlap with anything else
+    // => Output is 17-20 since it doesnt overlap with anything else
     // Everything else overlaps with something else, so can be discounted :)
+
+    public static void main(String[] args) {
+
+        Range r = new Range(0, 10);
+        Range s = new Range("8-15");
+        Range t = new Range("17-18");
+        Range u = new Range("22-30");
+        Range v = new Range("28-40");
+        Range w = new Range("11-19");
+        Range x = new Range("13-18");
+        Range y = new Range("16-25");
+        Range z = new Range("19-25");
+        Range z1 = new Range("19-22");
+        Range z2 = new Range("15-24");
+        Range z3 = new Range("19-29");
+
+        ArrayList<Range> ranges = new ArrayList<>();
+        ranges.add(r);
+        ranges.add(s);
+        ranges.add(t);
+        ranges.add(u);
+        ranges.add(v);
+//        ranges.add(w);
+//        ranges.add(x);
+//        ranges.add(y);
+        ranges.add(z);
+        ranges.add(z1);
+//        ranges.add(z2);
+        ranges.add(z3);
+        System.out.println(ranges);
+        // need to sort this according to the start times, and
+        // then arrange them. Didnt know this was NP
+        // what use is sorting? why do we need it?
+        // Sorting might help us do fewer comparisons for overlap
+
+        ranges = Range.sortRanges(ranges);
+        System.out.println(ranges);
+
+        Range OverlappedRanges = new Range("0-0");
+        int runs = 0;
+        Set<Range> rangeSet = new HashSet<>();
+        LinkedHashMap<Range, Integer> rangeIntegerHashMap = new LinkedHashMap<>();
+        for (int i = 0; i < ranges.size(); i++) {
+            // definitely not n^2, i dont even think this is n logn, it seems
+            // to be much lesser
+            int count = 0;
+            int j = i + 1;
+            Range range = ranges.get(i);
+            if (i != 0 && range.isOverlapping(ranges.get(i - 1))) {
+                // WHY? why is it that we check only one back, while we check so many forward
+                // cuz if we check one back, and its not overlapping, it can NEVER
+                // be overlapping with anything before that.
+                count++;
+                runs++;
+            }
+            while (j < ranges.size() && range.isOverlapping(ranges.get(j))) {
+                j++;
+                count++;
+                runs++;
+            }
+            // this is almost the solution I gave for Google
+            rangeIntegerHashMap.put(range, count);
+            if (count == 0) {
+                // means no overlap, can put in
+                rangeSet.add(range);
+            }
+            // TODO: heres where the optimization part comes in.
+        }
+        System.out.println(rangeSet);
+        System.out.println(runs);
+    }
 }
