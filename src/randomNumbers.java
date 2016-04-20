1a19016fc1b155064cbdfd2905653e422a40c9e4
Sai Pc <schand31@asu.edu>
2016-04-19 23:52:53 -0700

    random 5s

--- src/randomNumbers.java
+++ src/randomNumbers.java
@@ -1,14 +1,32 @@
+import java.util.Arrays;
+
 /**
  * Created by schandramouli on 4/19/16.
  */
 public class randomNumbers {
     public static void main(String[] args) {
-
+        int[] arrayTimes = new int[5];
+        for (int i = 0; i < 100; i++) {
+            int n = rand5();
+            System.out.println(n);
+            try {
+                Thread.sleep(1000);                 //1000 milliseconds is one second.
+            } catch(InterruptedException ex) {
+                Thread.currentThread().interrupt();
+            }
+            arrayTimes[n]++;
+        }
+        System.out.println(Arrays.toString(arrayTimes));
     }
 
     public static int rand5() {
         // returns a no between 1 and 5 inclusive,
         // with equal probability
-        System.out.println();
+        // --> By definition, having equal
+        // probability makes it non-random, lol.
+        // lets use the current system time
+        // as the seed
+        long n = System.currentTimeMillis();
+        return (int)n % 5;
     }
 }
