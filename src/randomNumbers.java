57b2a5ce315db9cae036ff9af3736eeda1cba9a7
Sai Pc <schand31@asu.edu>
2016-04-19 23:40:09 -0700

    rand5

---
+++ src/randomNumbers.java
@@ -0,0 +1,14 @@
+/**
+ * Created by schandramouli on 4/19/16.
+ */
+public class randomNumbers {
+    public static void main(String[] args) {
+
+    }
+
+    public static int rand5() {
+        // returns a no between 1 and 5 inclusive,
+        // with equal probability
+        System.out.println();
+    }
+}
