20d1fcec74ef24175eebfeda82c78fe886da2044
Sai Chandramouli <schand31@asu.edu>
2016-09-09 23:43:55 -0700

    pretty sure i got the impl right, need to get the printing right

--- src/AlgoStrings/Trie.java
+++ src/AlgoStrings/Trie.java
@@ -1,9 +1,57 @@
 package AlgoStrings;
 
+import java.util.HashMap;
+
 /**
  * Created by schandramouli on 9/9/16.
  */
 public class Trie {
+    TrieNode root;
     // this will have the methods to traverse, insert, search etc.
     // It is similar to a linkedlist or binarytree implementation.
+
+    public Trie() {
+        this.root = new TrieNode('^');
+    }
+
+    public Trie(TrieNode root) {
+        this.root = root;
+    }
+
+    public void insertWord(String word, TrieNode n) {
+        int wordLength = word.length();
+        System.out.println("Word is " + word + " and node is " + n);
+        if (wordLength == 0) {
+            return;
+        }
+        char c = word.charAt(0);
+        if (n.hasChild(c)) {
+            // continue traversing its children
+            insertWord(word.substring(1), n.getChild(c));
+        } else {
+            // insert the character and move down
+            TrieNode child = new TrieNode(c);
+            System.out.println("Inserting " + c);
+            n.insertChild(c, child);
+            // recursively populate its children.
+            insertWord(word.substring(1), child);
+        }
+
+    }
+
+    @Override
+    public String toString() {
+        return "Trie{" +
+                "root=" + root +
+                '}';
+    }
+
+    public static void main(String[] args) {
+        Trie t = new Trie();
+        System.out.println(t);
+        t.insertWord("tree", t.root);
+        System.out.println(t);
+        t.insertWord("trap", t.root);
+        System.out.println(t);
+    }
 }
--- src/AlgoStrings/TrieNode.java
+++ src/AlgoStrings/TrieNode.java
@@ -7,7 +7,7 @@ import java.util.HashMap;
  */
 public class TrieNode {
     char c;
-    HashMap<Character, TrieNode> trieNodeHashMap = new HashMap<>();
+    HashMap<Character, TrieNode> children = new HashMap<>();
 
     public TrieNode() {
         this.c = '$';
@@ -21,7 +21,36 @@ public class TrieNode {
         return c;
     }
 
+    public HashMap<Character, TrieNode> getChildren() {
+        return children;
+    }
+
     public boolean isLeaf() {
-        return c == '$';
+        return this.getChildren().size() == 0;
+    }
+
+    public boolean hasChild(char c) {
+        return children.containsKey(c);
+    }
+
+    public TrieNode getChild(char c) {
+        return children.get(c);
+    }
+
+    public void insertChild(char c, TrieNode n) {
+        children.put(c, n);
+    }
+
+    @Override
+    public String toString() {
+        String s = "\n";
+        s += "TrieNode{";
+        s += "c=" + c;
+        HashMap<Character, TrieNode> children = this.getChildren();
+        for (TrieNode child : children.values()) {
+            s += ", children=" + child.toString();
+        }
+        s += "}";
+        return s;
     }
 }
