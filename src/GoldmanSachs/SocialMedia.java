e29eaaf59d5458df14238381ddff47c3decedcae
Sai Chandramouli <schand31@asu.edu>
2016-09-13 22:30:39 -0700

    finished

--- src/GoldmanSachs/SocialMedia.java
+++ src/GoldmanSachs/SocialMedia.java
@@ -1,7 +1,10 @@
 package GoldmanSachs;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.jar.Pack200;
 
 /**
  * Created by schandramouli on 9/13/16.
@@ -38,11 +41,32 @@ public class SocialMedia implements Comparable{
         Collections.sort(messages);
         System.out.println(messages);
 
-        // need 3 hashmaps to answer each of the three questions
+        // need a hashmaps to answer each of the three questions
         // 1. Whose messages got forwarded most
         // 2. Which time interval had most messages
         // 3. Which message is sent very frequently
         // good thing is all these can be finished in 1 loop :O
+        HashMap<String, Integer> messagesForwarded = new HashMap<>();
+        HashMap<String, Integer> messageFrequency = new HashMap<>();
+        // trick: using good-ole array is sufficient for timeIntervals :D
+        int[] timeIntervals = new int[24];
+        for (SocialMedia sm : messages) {
+            // first time :D easiest
+            String time = sm.time;
+            String message = sm.message;
+            String name = sm.name;
+            String hours = sm.time.substring(0,2);
+            timeIntervals[Integer.parseInt(hours)]++;
+            // next messages, second-easiest
+            if (messageFrequency.containsKey(message)) {
+                messageFrequency.put(message, messageFrequency.get(message) + 1);
+            } else {
+                messageFrequency.put(message, 0);
+            }
+        }
+
+        System.out.println(Arrays.toString(timeIntervals));
+        System.out.println(messageFrequency);
 
     }
 
