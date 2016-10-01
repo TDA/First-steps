e4ea85f97481267773100587d2e2aada3d73ff23
Sai Chandramouli <schand31@asu.edu>
2016-09-30 21:20:23 -0700

    finished the base for num->string, have edge cases

--- src/NumberToStringRepresentation.java
+++ src/NumberToStringRepresentation.java
@@ -6,53 +6,32 @@ import java.util.Scanner;
  */
 public class NumberToStringRepresentation {
     public static void main(String[] args) {
-        HashMap<Integer, String> onesAndHundreds = new HashMap<>();
-        onesAndHundreds.put(0, "");
-        onesAndHundreds.put(1, "one");
-        onesAndHundreds.put(2, "two");
-        onesAndHundreds.put(3, "three");
-        onesAndHundreds.put(4, "four");
-        onesAndHundreds.put(5, "five");
-        onesAndHundreds.put(6, "six");
-        onesAndHundreds.put(7, "seven");
-        onesAndHundreds.put(8, "eight");
-        onesAndHundreds.put(9, "nine");
+        String[] onesAndHundreds = {"", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
 
-
-
-        HashMap<Integer, String> tens = new HashMap<>();
-        tens.put(0, "");
-        tens.put(1, "ten");
-        tens.put(2, "twenty");
-        tens.put(3, "thirty");
-        tens.put(4, "forty");
-        tens.put(5, "fifty");
-        tens.put(6, "sixty");
-        tens.put(7, "seventy");
-        tens.put(8, "eighty");
-        tens.put(9, "ninety");
+        String[] tens = {"", "ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"};
 
         Scanner s = new Scanner(System.in);
         int num = s.nextInt();
         int count = 0;
-        String[] appendices = {"", "Thousand", "Million", "Billion"};
+        String[] appendices = {"", "thousand", "million", "billion"};
         StringBuilder number = new StringBuilder("");
         while (num > 0) {
             // get a 3 digit slice
             int digitSlice = num % 1000;
             num = num / 1000;
-            number.insert(0, appendices[count++]);
-            for (int i = 0; i < 3; i++) {
+            number.insert(0, appendices[count++] + " ");
+            for (int i = 0; i < 3 && digitSlice > 0; i++) {
                 int subDigit = digitSlice % 10;
                 digitSlice = digitSlice / 10;
                 if (i == 1) {
-                    number.insert(0, tens.get(subDigit));
-                    System.out.println(tens.get(subDigit));
+                    number.insert(0, tens[subDigit] + " ");
+                    System.out.println(tens[subDigit]);
                 } else {
-                    number.insert(0, onesAndHundreds.get(subDigit));
-                    System.out.println(onesAndHundreds.get(subDigit));
+                    String h = onesAndHundreds[subDigit] + " ";
+                    if (i == 2) h += "hundred and ";
+                    number.insert(0, h);
+                    System.out.println(onesAndHundreds[subDigit]);
                 }
-
             }
         }
 
