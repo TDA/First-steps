531df23525d073f9dacc7c5da66230a3b5d9da27
Sai Chandramouli <schand31@asu.edu>
2016-10-04 23:53:13 -0700

    added fb interview questions

---
+++ src/FacebookInterviews/RatiosOfNumbers.java
@@ -0,0 +1,14 @@
+package FacebookInterviews;
+
+/**
+ * Created by schandramouli on 10/4/16.
+ */
+public class RatiosOfNumbers {
+    public static void main(String[] args) {
+        // input is something like:
+        // A B 1.0
+        // B C 0.8
+        // C D 0.5
+        // o/p is to answer "What is A/D?"
+    }
+}
