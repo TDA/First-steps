18ca59bc6d65b40491dd957a99132d0c4f8f346c
Sai Chandramouli <schand31@asu.edu>
2016-10-07 00:18:18 -0700

    fixed according to sanjanas suggestions

--- src/FacebookInterviews/NumberToStringRepresentation.java
+++ src/FacebookInterviews/NumberToStringRepresentation.java
@@ -16,12 +16,6 @@ public class NumberToStringRepresentation {
         Scanner s = new Scanner(System.in);
         int num = s.nextInt();
         StringBuilder number = new StringBuilder("");
-        // special cases first
-        int tensAndOnes = num % 100;
-        if (tensAndOnes > 10 && tensAndOnes < 20) {
-            number.insert(0, doubleDigits[tensAndOnes % 10]);
-            num = num - tensAndOnes;
-        }
         int count = 0;
         String[] appendices = {"", "thousand", "million", "billion"};
         // pretty sure this can be written in a better way,
@@ -29,8 +23,14 @@ public class NumberToStringRepresentation {
         while (num > 0) {
             // get a 3 digit slice
             int digitSlice = num % 1000;
-            num = num / 1000;
             number.insert(0, appendices[count++] + " ");
+            num = num / 1000;
+            // special cases first
+            int tensAndOnes = digitSlice % 100;
+            if (tensAndOnes > 10 && tensAndOnes < 20) {
+                number.insert(0, doubleDigits[tensAndOnes % 10] + " ");
+                digitSlice = digitSlice - tensAndOnes;
+            }
             for (int i = 0; i < 3 && digitSlice > 0; i++) {
                 int subDigit = digitSlice % 10;
                 digitSlice = digitSlice / 10;
