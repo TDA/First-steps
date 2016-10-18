769be8f70e756b5d3d5d8ad5ec117a2b767e4f0e
Sai Chandramouli <schand31@asu.edu>
2016-10-17 22:56:25 -0700

    trying a google q after a long time

--- src/GoogleInterviews/FirstInterview.java
+++ src/GoogleInterviews/FirstInterview.java
@@ -1,5 +1,7 @@
 package GoogleInterviews;
 
+import java.util.ArrayList;
+
 /**
  * Created by schandramouli on 12/2/15.
  */
@@ -10,7 +12,34 @@ public class FirstInterview {
 //    [1] -> b1, b2
 //    [2] -> c1, c3
 //    => a1, b1, c1, a2, b2, a3, c3 should be the output :)
-    public static void main(String[] args) {
 
+    // LoL stands for List of lists :P :D
+    ArrayList<ArrayList<String>> LoL = new ArrayList<>();
+
+    public static void main(String[] args) {
+        ArrayList<String> l1 = new ArrayList<>();
+        ArrayList<String> l2 = new ArrayList<>();
+        ArrayList<String> l3 = new ArrayList<>();
+        l1.add("a1");l1.add("a2");l1.add("a3");l1.add("a4");l1.add("a5");
+        l2.add("b1");l2.add("b2");l2.add("b3");
+        l3.add("c1");l3.add("c2");l3.add("c3");l3.add("c4");
+        System.out.println(l1);
+        System.out.println(l2);
+        System.out.println(l3);
+        FirstInterview f = new FirstInterview();
+        f.LoL.add(l1);
+        f.LoL.add(l2);
+        f.LoL.add(l3);
     }
+
+    public String next() {
+        System.out.println();
+        return "";
+    }
+
+    public boolean hasNext() {
+        System.out.println();
+        return true;
+    }
+
 }
