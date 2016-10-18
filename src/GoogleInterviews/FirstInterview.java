0cebf45366e9f024e6d3a4919486579f61c03b26
Sai Chandramouli <schand31@asu.edu>
2016-10-18 01:05:28 -0700

    this was almost too easy

--- src/GoogleInterviews/FirstInterview.java
+++ src/GoogleInterviews/FirstInterview.java
@@ -8,13 +8,26 @@ import java.util.ArrayList;
 public class FirstInterview {
     // given an iterator of iterators, implement next() and hasNext()
     // to get the iterators in alternating fashion
-//    [0] -> a1, a2, a3
-//    [1] -> b1, b2
-//    [2] -> c1, c3
-//    => a1, b1, c1, a2, b2, a3, c3 should be the output :)
+    // [0] -> a1, a2, a3
+    // [1] -> b1, b2
+    // [2] -> c1, c2, c3
+    // => a1, b1, c1, a2, b2, c2 a3, c3 should be the output :)
+
+    // tbh, now that i think about this, this is just a ragged array
+    // so basically needs a row counter and a column counter
+    // the m * n is obtained by the .size(), like so:
+    // m = mainList.size()
+    // n = subList.size()
+
+    // so then, it would be a simple check whether the column counter (cc)
+    // is <= n, and the row counter (rc) is <= m and retrieve the value.
+    // if cc overflows n, then move on to the next rc, if rc overflows m,
+    // reset back to 0, and move to next cc.
 
     // LoL stands for List of lists :P :D
     ArrayList<ArrayList<String>> LoL = new ArrayList<>();
+    int nextList;
+    int nextElement;
 
     public static void main(String[] args) {
         ArrayList<String> l1 = new ArrayList<>();
@@ -27,9 +40,18 @@ public class FirstInterview {
         System.out.println(l2);
         System.out.println(l3);
         FirstInterview f = new FirstInterview();
-        f.LoL.add(l1);
-        f.LoL.add(l2);
-        f.LoL.add(l3);
+        f.addList(l1);
+        f.addList(l2);
+        f.addList(l3);
+    }
+
+    private void addList(ArrayList<String> l) {
+        LoL.add(l);
+    }
+
+    public FirstInterview() {
+        this.nextList = 0;
+        this.nextElement = 0;
     }
 
     public String next() {
