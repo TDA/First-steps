6f44c4f5fe0cfb051dfb195a94de9f0a19eb3469
Sai Chandramouli <schand31@asu.edu>
2016-11-05 20:07:28 -0700

    easier string reversal

--- src/TwitterInterviews/SimpleExpressionTrees.java
+++ src/TwitterInterviews/SimpleExpressionTrees.java
@@ -11,6 +11,11 @@ public class SimpleExpressionTrees {
         String s = "(AB) C((D E)F)";
         s = s.replaceAll("\\s+", "");
         System.out.println(s);
+        System.out.println(reverse1(s));
+        System.out.println(reverse2(s));
+    }
+
+    public static String reverse1(String s) {
         Stack<Character> charStack = new Stack<>();
         for (char c : s.toCharArray()) {
             if (c == '(') {
@@ -25,6 +30,21 @@ public class SimpleExpressionTrees {
             char c = charStack.pop();
             rS += c;
         }
-        System.out.println(rS);
+        return rS;
+    }
+
+    public static String reverse2(String s) {
+        String rS2 = "";
+        for (int i = s.length() - 1; i > -1; i--) {
+            // reversing made easier?
+            char c = s.charAt(i);
+            if (c == '(') {
+                c = ')';
+            } else if (c == ')') {
+                c = '(';
+            }
+            rS2 += c;
+        }
+        return rS2;
     }
 }
