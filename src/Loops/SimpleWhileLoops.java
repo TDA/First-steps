839f9dc556e3621718d76a9d05d6cdae795dfcb2
Sai Chandramouli <schand31@asu.edu>
2017-04-26 11:38:17 -0700

    simple while loops

---
+++ src/Loops/SimpleWhileLoops.java
@@ -0,0 +1,14 @@
+package Loops;
+
+/**
+ * Created by schandramouli on 4/26/17.
+ */
+public class SimpleWhileLoops {
+    public static void main(String[] args) {
+      int i = 0;
+        while (i < 11) {
+          System.out.println(i);
+          i++;
+        }
+    }
+}
