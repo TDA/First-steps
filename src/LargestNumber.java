64f1cb6f809d6bdc3663043fb7e1cf5735ceaa33
Sai Chandramouli <schand31@asu.edu>
2017-05-10 21:26:34 -0700

    adding the first digit finder

--- src/LargestNumber.java
+++ src/LargestNumber.java
@@ -1,11 +1,22 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+
 /**
  * Created by schandramouli on 5/30/17.
  */
 
 public class LargestNumber {
     public static void main(String args[]){
-        int[] input = {3, 30, 34, 5, 9};
-        int k = 0;
 
+    }
+
+    private static int getFirstDigit(int number) {
+        int digit = number;
+        while (number > 0) {
+            digit = number % 10;
+            number = number / 10;
+        }
+        return digit;
     }
 }
